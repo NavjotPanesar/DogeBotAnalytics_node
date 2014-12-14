fe4594159c8552d0d79586ab3a8d41bac4c5f017
Navjot Panesar] <NavjotSPanesar@gmail.com>
2014-12-13 22:25:14 -0500

    I always forgot to remove the angle brackets

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 //Mongo
 var mongoose = require('mongoose');
-mongoose.connect('mongodb://<admin>:<system32>@ds043947.mongolab.com:43947/dogeanalytics');
+mongoose.connect('mongodb://admin:system32@ds043947.mongolab.com:43947/dogeanalytics');
 
 var index = require('./routes/');
 
