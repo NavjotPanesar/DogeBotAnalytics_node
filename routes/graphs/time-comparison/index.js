99e9c964a3f1c047203108a618de4820848fffb7
Navjot Panesar <NavjotSPanesar@gmail.com>
2014-12-18 20:56:50 -0500

    Added data route to graphs

--- routes/graphs/time-comparison/index.js
+++ routes/graphs/time-comparison/index.js
@@ -4,18 +4,26 @@ var app = module.exports = express();
 var Command = require('../../commands/command.model.js');
 /* GET pretty html graph */
 app.get('/', function(req, res) {
-    getGraphData(function(graphData){
+    getGraphData(function(commandList, timeList){
+        var graphData = {'labels': JSON.stringify(commandList), 'data': JSON.stringify(timeList)};
         res.render('time-comparison', graphData);
     });
 });
 
 /* GET basic graph, only image */
 app.get('/image/:width/:height', function(req, res) {
-    getGraphData(function(graphData){
-            graphData.width = req.params.width;
-            graphData.height = req.params.height;
+    getGraphData(function(commandList, timeList){
+            var graphData = {'labels': JSON.stringify(commandList), 'data': JSON.stringify(timeList), 'width': req.params.width, 'height': req.params.height};
             res.render('time-comparison-plain', graphData);
         });
+});
+
+/* GET graph data only */
+app.get('/data/', function(req, res) {
+    getGraphData(function(commandList, timeList){
+            var graphData = {'labels': commandList, 'data': timeList};
+            res.send(graphData);
+      });
 });
 
 
@@ -28,7 +36,6 @@ var getGraphData = function(callback){
                     commandList.push(summaryRow._id);
                     timeList.push(summaryRow.averageTime);
                 }
-                var graphData = {'labels': JSON.stringify(commandList), 'data': JSON.stringify(timeList)};
-                callback(graphData);
+                callback(commandList, timeList);
             });
 }
